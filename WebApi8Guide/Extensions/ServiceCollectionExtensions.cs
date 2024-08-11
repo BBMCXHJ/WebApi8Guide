9f6bef2b09f4e542e1301f72db140b5012436663
BBMCXHJ <1826382358@qq.com>
2024-08-11 20:14:06 +0800

    第一次优化依赖注入代码逻辑

--- WebApi8Guide/Extensions/ServiceCollectionExtensions.cs
+++ WebApi8Guide/Extensions/ServiceCollectionExtensions.cs
@@ -5,40 +5,49 @@ namespace WebApi8Guide.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddServicesFromAssembly<T>(this IServiceCollection services, Assembly assembly)
+        public static void AddServicesFromAssemblies(this IServiceCollection services, List<Type> serviceTypes)
         {
-            // 获取所有实现了T接口的类  
-            var serviceTypes = assembly.GetTypes()
-                .Where(t => t.IsClass && !t.IsAbstract && t.GetInterfaces().Contains(typeof(T)));
-
             foreach (var serviceType in serviceTypes)
             {
-                // 遍历服务类型的所有接口  
-                foreach (var interfaceType in serviceType.GetInterfaces())
-                {
-                    // 检查服务类是否有 SingletonServiceAttribute 特性  
-                    bool isSingleton = serviceType.GetCustomAttributes(typeof(SingletonServiceAttribute), false).Any();
+                var assembly = Assembly.GetAssembly(serviceType);
+                AddServicesFromAssembly(services, serviceType, assembly);
+            }
+        }
+        public static void AddServicesFromAssembly(this IServiceCollection services, Type serviceType, Assembly assembly)
+        {
+            // 获取对应接口的实现类  
+            var serviceClass = assembly.GetTypes()
+                                     .Where(t => t.IsClass && !t.IsAbstract && t.GetInterfaces().Contains(serviceType));
 
-                    if (isSingleton)
+            foreach (var service in serviceClass)
+            {
+                // 获取当前服务类实现的所有接口，并遍历它们 
+                //foreach (var interfaceType in service.GetInterfaces())
+                //{
+
+                // 检查服务类是否有 SingletonServiceAttribute 特性  
+                bool isSingleton = service.GetCustomAttributes(typeof(SingletonServiceAttribute), false).Any();
+
+                if (isSingleton)
+                {
+                    // 注册为单例(Singleton)
+                    services.AddSingleton(serviceType, service);
+                }
+                else
+                {
+                    bool isTransient = service.GetCustomAttributes(typeof(TransientServiceAttribute), false).Any();
+                    if (isTransient)
                     {
-                        // 注册为单例(Singleton)
-                        services.AddSingleton(interfaceType, serviceType);
+                        // 注册为瞬时(Transient)
+                        services.AddTransient(serviceType, service);
                     }
                     else
                     {
-                        bool isTransient = serviceType.GetCustomAttributes(typeof(TransientServiceAttribute), false).Any();
-                        if (isTransient)
-                        {
-                            // 注册为瞬时(Transient)
-                            services.AddTransient(interfaceType, serviceType);
-                        }
-                        else 
-                        {
-                            // 注册为作用域(Scoped)  
-                            services.AddScoped(interfaceType, serviceType);
-                        }                    
+                        // 注册为作用域(Scoped)  
+                        services.AddScoped(serviceType, service);
                     }
                 }
+                //}
             }
         }
     }
--- WebApi8Guide/Program.cs
+++ WebApi8Guide/Program.cs
@@ -16,14 +16,11 @@ namespace WebApi8Guide
             // Add services to the container.
             builder.Services.Configure<AppSetting>(builder.Configuration.GetSection("ConnectionStrings"));
 
-            // ****** 依赖注入：手动注入服务，并且指定服务的生命周期 ******
-            //builder.Services.AddSingleton<IAppSettingService, AppSettingService>();
-            //builder.Services.AddScoped<ITestService, TestService>();
-
-            // ****** 依赖注入：使用反射依次注册服务，根据每个服务上方的自定义属性[SingletonService]来判断该服务的生命周期 ****** 
-            builder.Services.AddServicesFromAssembly<IAppSettingService>(Assembly.GetAssembly(typeof(IAppSettingService)));
-            builder.Services.AddServicesFromAssembly<ITestService>(Assembly.GetAssembly(typeof(ITestService)));
-            builder.Services.AddServicesFromAssembly<IProductService>(Assembly.GetAssembly(typeof(IProductService)));
+            builder.Services.AddServicesFromAssemblies(new List<Type>{
+                typeof(IAppSettingService),
+                typeof(ITestService),
+                typeof(IProductService)
+            });
 
             builder.Services.AddControllers();
 
